3abbf58b975097c1735a6ca3f4c62ccd83b34faf
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 21:18:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var katzDeliLine = [];
+
+function takeANumber(currLine,newCust) {
+  return `Welcome, ${newCust}. You are number ${custNum} in line.`
+}
+
+function nowServing() {
+  if (katzDeliLine>0) {
+    return 
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine() {
+  
+}
