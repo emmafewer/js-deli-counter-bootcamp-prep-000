c9bc89f6bd6a5908fbc45e74579bdd4e3dfa4d51
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 21:26:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ function nowServing(currLine) {
   if (currLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return 
+      for(var i = 0; i < NewLine.length; i++) {
+        line += (i + 1) + ". " + NewLine[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
 
 function currentLine(NewLine) {
     var line = []
