7bfa95ab0f27a03acc6c7ce08ff780264034389d
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 21:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,17 @@ function nowServing(currLine) {
     currLine.splice(0, 1);
     return "Currently serving " + name + ".";
   }
+}
 
 function currentLine(NewLine) {
     var line = []
     if (NewLine.length === 0) {
       return "The line is currently empty."
     } else {
+      for(var i = 0; i < NewLine.length; i++) {
+        line += (i + 1) + ". " + NewLine[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
 }
